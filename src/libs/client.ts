04efa9a07c09364796bc7b14c3a0cc72736499fd
yugo-yamamoto-genova <yugo.yamamoto@genova.co.jp>
2022-11-16 17:52:57 +0900

    ✨ microCMSのAPIと連携

---
+++ src/libs/client.ts
@@ -0,0 +1,7 @@
+import { createClient } from "microcms-js-sdk"; //ES6
+
+// Initialize Client SDK.
+export const client = createClient({
+  serviceDomain: "study-microcms-blog",
+  apiKey: process.env.API_KEY ? process.env.API_KEY : "",
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-import type { NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
+import { client } from "src/libs/client";
 
-const Home: NextPage = () => {
+const endpoint = "api/vi/";
+
+type Blog = {
+  title: string;
+  body: string;
+};
+
+const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
+  console.log(props.contents[0].title);
+
   return <div className="text-blue-500">Hello!</div>;
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  return {
+    props: data,
+  };
+};
