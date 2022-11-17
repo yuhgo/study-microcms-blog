7de60521a728313228bfdac1e2bfe1911d1db925
yugo-yamamoto-genova <yugo.yamamoto@genova.co.jp>
2022-11-17 12:44:59 +0900

    ✨ 一覧ページと詳細ページを作成

---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,47 @@
+import { MicroCMSContentId, MicroCMSDate } from "microcms-js-sdk";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { client } from "src/libs/client";
+import { Blog } from "src/pages";
+
+type Props = Blog & MicroCMSContentId & MicroCMSDate;
+
+const BlogId: NextPage<Props> = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <time>{props.publishedAt}</time>
+      <div dangerouslySetInnerHTML={{ __html: props.body }} />
+    </div>
+  );
+};
+
+export default BlogId;
+
+export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
+  const data = await client.getList({ endpoint: "blog" });
+  const ids = data.contents.map((content) => `/blog/${content.id}`);
+
+  return {
+    paths: ids,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, { id: string }> = async (
+  ctx
+) => {
+  if (!ctx.params) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const data = await client.getListDetail<Blog>({
+    endpoint: "blog",
+    contentId: ctx.params.id,
+  });
+
+  return {
+    props: data,
+  };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,40 @@
 import { MicroCMSListResponse } from "microcms-js-sdk";
 import type { GetStaticProps, NextPage } from "next";
+import Link from "next/link";
 import { client } from "src/libs/client";
 
 const endpoint = "api/vi/";
 
-type Blog = {
+export type Blog = {
   title: string;
   body: string;
 };
 
-const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
-  console.log(props.contents[0].title);
+type Props = MicroCMSListResponse<Blog>;
 
-  return <div className="text-blue-500">Hello!</div>;
+const Home: NextPage<Props> = (props) => {
+  return (
+    <div>
+      <p>{`記事の総数: ${props.totalCount}件`}</p>
+      <ul>
+        {props.contents.map((content) => {
+          return (
+            <li key={content.id}>
+              <Link href={`/blog/${content.id}`}>
+                <a>{content.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
 
-export const getStaticProps: GetStaticProps = async () => {
-  const data = await client.get({ endpoint: "blog" });
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList<Blog>({ endpoint: "blog" });
 
   return {
     props: data,
